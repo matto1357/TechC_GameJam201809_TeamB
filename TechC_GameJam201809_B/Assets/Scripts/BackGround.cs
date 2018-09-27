ca06ebea2f44c4f29908a1afa2d20e03eab45d6f
高田将志 <masapo.0315@icloud.com>
2018-09-27 23:24:31 +0900

    とりあえず背景を動かし一定の位置で止まるようにした。

---
+++ TechC_GameJam201809_B/Assets/Scripts/BackGround.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour {
+
+    //minの座標からmaxの座標まで背景が動く
+    float min = -7;
+    float max = 7;
+
+    //背景の動くスピード（調整可）
+    [SerializeField]
+    public float speed;
+	
+	// Update is called once per frame
+	void Update () {
+
+        TransPostion();
+	}
+
+    public void TransPostion()
+    {
+
+        transform.position = new Vector2(0, Mathf.Lerp(min, max, Time.time * speed));
+
+    }
+}
