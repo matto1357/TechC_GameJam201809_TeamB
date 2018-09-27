f185a84fca85cb8804bb10a7b8fbc27194aedeb8
松田幸介 <darby8928@gmail.com>
2018-09-27 23:02:42 +0900

    たこ足とシャチ尾びれの生成スクリプトを作成

---
+++ TechC_GameJam201809_B/Assets/Scripts/ObjectDelete.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDelete : MonoBehaviour {
+
+    GameObject obj;
+    ActionControler actionControler;
+	// Use this for initialization
+	void Start () {
+        obj = GameObject.Find("GameObject");
+        actionControler = obj.GetComponent<ActionControler>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        DeleteThisObject();
+	}
+    //このobjectをdeleteする関数
+    private void DeleteThisObject()
+    {
+        if(this.transform.position.x <= -10)//position.xが一定位置を過ぎたら
+        {
+
+            switch (this.gameObject.tag)
+            {
+                case "Octopus":
+                    actionControler.octopusLeg.Remove(this.gameObject);
+                    break;
+                case "KillerWhale":
+                    actionControler.killerWhaleTailFin.Remove(this.gameObject);
+                    break;
+            }
+            //Debug.Log(actionControler.octopusLeg[0]);
+            //Debug.Log(actionControler.killerWhaleTailFin.Count);
+            Destroy(this.gameObject);
+        }
+    }
+}
