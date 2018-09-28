235425fcf76549a905309fef4af96ef68461f15e
高田将志 <masapo.0315@icloud.com>
2018-09-28 12:23:03 +0900

    ワカメを右から左へスクロールできるようにした

---
+++ TechC_GameJam201809_B/Assets/Scripts/ObjectWideScroll.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectWideScroll : MonoBehaviour {
+
+    [SerializeField]
+    public float speed;
+
+	// Update is called once per frame
+	void Update () {
+        ObjectScroll();
+	}
+
+    public void ObjectScroll()
+    {
+        this.transform.position -= new Vector3(speed, 0, 0);
+    }
+}
