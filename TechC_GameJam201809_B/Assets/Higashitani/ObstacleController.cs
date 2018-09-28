b87b7bbd2f1689792fadeb35e95f8a5c71154e02
dependd <darby8928@gmail.com>
2018-09-28 10:14:33 +0900

    わかめshader　BGM・SEのコントローラー　ステージエディター途中　タイトル用ガウスブラー　障害物類

---
+++ TechC_GameJam201809_B/Assets/Higashitani/ObstacleController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour {
+    [SerializeField]
+    List<GameObject> Objects = new List<GameObject>();
+    [SerializeField]
+    StageData sData;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for(int i = 0; i < sData.listObjects.Count; i++)
+        {
+            //if (sData.listObjects[i].spornNum)
+            //{
+
+            //}
+        }
+	}
+}
---
+++ TechC_GameJam201809_B/Assets/Higashitani/TitleController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleController : MonoBehaviour {
+
+    [SerializeField]
+    GameObject pauseUI;
+
+    [SerializeField]
+    Gauss gauss;
+
+    bool pauseFlag = false;
+    float intencity = 0;
+    float prevTime;
+
+    void Update()
+    {
+        var deltaTime = Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            pauseFlag = !pauseFlag;
+        }
+        if (pauseFlag)
+        {
+            pauseUI.SetActive(true);
+            intencity += deltaTime * 8;
+        }
+        else
+        {
+            pauseUI.SetActive(false);
+            intencity -= deltaTime * 8;
+        }
+        intencity = Mathf.Clamp01(intencity);
+        gauss.Resolution = (int)(intencity * 10);
+    }
+}
