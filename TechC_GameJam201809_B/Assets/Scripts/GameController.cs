4a726b9e4cc95f4d7514001bb531658d95092363
Hirokawa <17d3170021hm@edu.tech.ac.jp>
2018-09-28 09:40:23 +0900

    ゲームコントローラー、足がボタンで動くスクリプト

---
+++ TechC_GameJam201809_B/Assets/Scripts/GameController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    //変数エリア
+    private float gameTime = 120f;          //ゲーム残り時間
+    private int score = 0;                  //スコア
+
+    enum gameState                          //ゲームの状態がどこにいるか
+    {
+        Title = 0,
+        Game,
+        Result
+    }
+
+    private gameState _gameState = 0;       //いーなむ
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        TimeLapse();
+	}
+
+    private void TimeLapse()                //タイム経過させる
+    {
+        gameTime -= Time.deltaTime;
+        if (gameTime <= 0)
+        {
+            //ゲーム終了処理
+        }
+    }
+
+    public void AddScore(int value = 1)     //スコア加算
+    {
+        score += value;
+    }
+
+    public void DeleteScore(int value = 1)  //スコア減算
+    {
+        score -= value;
+        if(score < 0)
+        {
+            score = 0;
+        }
+    }
+}
