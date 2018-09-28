bf342bb69497b0442ba263be07b9ae61bbb2ee34
高田将志 <masapo.0315@icloud.com>
2018-09-28 17:01:01 +0900

    たこ足とシャチ尾に当たり判定はつけた。まだ、ボールを飛ばせない。

--- TechC_GameJam201809_B/Assets/Scripts/GetKey.cs
+++ TechC_GameJam201809_B/Assets/Scripts/GetKey.cs
@@ -6,17 +6,17 @@ public class GetKey : MonoBehaviour {
 
     //Inspectorで好きなボタンに変えられるようにする
     [SerializeField]
-    KeyCode key1A;  // 1Pのアクションボタン
+    public KeyCode key1A;  // 1Pのアクションボタン
     [SerializeField]
-    KeyCode key1U;  // 1Pの↑ボタン
+    public KeyCode key1U;  // 1Pの↑ボタン
     [SerializeField]
-    KeyCode key1D;  // 1Pの↓ボタン
+    public KeyCode key1D;  // 1Pの↓ボタン
     [SerializeField]
-    KeyCode key2A;  // 2Pのアクションボタン
+    public KeyCode key2A;  // 2Pのアクションボタン
     [SerializeField]
-    KeyCode key2U;  // 2Pの↑ボタン
+    public KeyCode key2U;  // 2Pの↑ボタン
     [SerializeField]
-    KeyCode key2D;  // 2Pの↓ボタン
+    public KeyCode key2D;  // 2Pの↓ボタン
 	
 	// Update is called once per frame
 	void Update () {
@@ -24,7 +24,7 @@ public class GetKey : MonoBehaviour {
         Comand();
 	}
 
-    public void Comand()
+    void Comand()
     {
         //各コマンドボタンの処理
         if (Input.GetKeyDown(key1A))
@@ -45,7 +45,7 @@ public class GetKey : MonoBehaviour {
         if (Input.GetKeyDown(key2A))
         {
             Debug.Log("key2A!");
-        } 
+        }
 
         if (Input.GetKey(key2U) && Input.GetKeyDown(key2A))
         {
@@ -56,6 +56,6 @@ public class GetKey : MonoBehaviour {
         {
             Debug.Log("key2D!");
         }
-
     }
+    
 }
